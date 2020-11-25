2b4e1d1025e3a890c54013d0ed0bba2a1cf6f36b
Your Name <jarblz@yandex.ru>
2020-11-25 18:26:54 +0500

    colorful circles with ui converted to python class

--- test1.py
+++ test1.py
@@ -1,15 +1,32 @@
 import random
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor, QBrush, QPen
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class GitCircles(QMainWindow):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(513, 482)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(20, 20, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Tap"))
+
+
+class GitCircles(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.flag = False
         self.s = []
         self.pushButton.clicked.connect(self.click)
@@ -18,10 +35,9 @@ class GitCircles(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            y = QColor.fromRgb(255, 255, 0)
-            qp.setBrush(QBrush(y))
-            qp.setPen(QPen(y))
-            for x, yy, r in self.s:
+            for x, yy, r, color in self.s:
+                qp.setBrush(QBrush(color))
+                qp.setPen(QPen(color))
                 qp.drawEllipse(x, yy, r, r)
             qp.end()
             self.flag = False
@@ -29,7 +45,8 @@ class GitCircles(QMainWindow):
     def click(self):
         self.flag = True
         x, y, r = random.randint(30, 400), random.randint(30, 400), random.randint(30, 100)
-        self.s.append((x, y, r))
+        color = QColor.fromRgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.s.append((x, y, r, color))
         self.repaint()
 
 
